f536550a3a725c1b661ea187e64dc59084c04aa7
mateusdreher <mateusdreher.dev@gmail.com>
2023-07-07 17:54:55 -0300

    refactor: ajdjust layout

--- src/components/List.js
+++ src/components/List.js
@@ -7,6 +7,7 @@ import ModalSuccess from '../components/ModalSucces';
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    flexDirection: "column",
     alignItems: "center",
     padding: "1rem",
     backgroundColor: "#273649",
@@ -15,36 +16,52 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",
   },
   imgContainer: {
-    width: "100px",
-    height: "80px",
+    width: "100%",
+    height: "200px",
     borderRadius: "10px",
     overflow: "hidden",
-    marginRight: "1rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: "1rem",
+    backgroundColor: "#fff",
   },
   img: {
     width: "100%",
     height: "100%",
-    objectFit: "cover",
+    objectFit: "scale-down",
   },
   name: {
     fontWeight: "bold",
     fontSize: "1.2rem",
     color: "#eda787",
+    textAlign: "center",
+    marginBottom: "0.5rem",
   },
   price: {
     fontWeight: "bold",
     fontSize: "1rem",
-    marginBottom: "1rem",
     color: "#e37846",
+    textAlign: "center",
+    marginBottom: "1rem",
+  },
+  buttonGroup: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "end",
+    width: "100%",
+    marginTop: "auto",
+    marginBottom: "15px", // Adicionar margem abaixo dos botões
   },
   linkBtn: {
     backgroundColor: "#eda787",
     color: "#fff",
     borderRadius: "10px",
     padding: "0.3rem 1rem",
-    marginRight: "0.5rem",
     textTransform: "none",
     fontSize: "0.8rem",
+    width: "45%", // Definir largura fixa para os botões
+    minHeight: "2.5rem", // Definir altura mínima para os botões
   },
   boughtBtn: {
     backgroundColor: "#273649",
@@ -54,21 +71,9 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid #eda787",
     textTransform: "none",
     fontSize: "0.8rem",
-  },
-  buttonGroup: {
-    display: "flex",
-    fontStyle: "bold"
-  },
-  title: {
-    color: "#eda787",
-    fontFamily: "DynaPuff",
-    fontSize: "2.5rem"
-  },
-  subtitle: {
-    color: "#eda787",
-    fontFamily: "DynaPuff",
-    fontSize: "0.9rem",
-    marginLeft: "0.5rem",
+    width: "45%", // Definir largura fixa para os botões
+    minHeight: "2.5rem", // Definir altura mínima para os botões
+
   },
   justifyCenter: {
     display: "flex",
@@ -76,20 +81,16 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "flex-end",
     marginBottom: "5rem",
   },
-  gridContainer: {
-    width: "80%",
-    paddingLeft: "10%",
-    display: "grid",
-    gridGap: "2rem",
-    [theme.breakpoints.down("xs")]: {
-      gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
-    },
-    [theme.breakpoints.up("sm")]: {
-      gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
-    },
-    [theme.breakpoints.up("md")]: {
-      gridTemplateColumns: "repeat(3, 1fr)",
-    },
+  logoContainer: {
+    display: "grid", 
+    gridTemplateColumns: "repeat(2)", 
+    alignItems: "center", 
+    justifyContent: "center",
+    [theme.breakpoints.down("sm")]: {
+      gridTemplateColumns: "1fr",
+      paddingTop: "0",
+      height: "200px"
+    }
   },
   descriptionDiv: {
     color: "#fff",
@@ -99,32 +100,72 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: "25%",
     textAlign: "justify",
     lineHeight: "1.5rem",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      paddingLeft: "1%",
+    },
+  },
+  title: {
+    color: "#eda787",
+    fontFamily: "DynaPuff",
+    fontSize: "2.8rem",
+    position: "absolute",
+    top: "3rem",
+    [theme.breakpoints.down("sm")]: {
+      position: "absolute",
+      top: "1rem",
+      marginLeft: "-1rem",
+    }
+  },
+  subtitle: {
+    color: "#eda787",
+    fontFamily: "DynaPuff",
+    fontSize: "1.2rem",
+    marginLeft: "0.5rem",
+    position: "absolute",
+    top: "8rem",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "-1rem",
+      top: "6rem",
+    }
   },
   flowerDiv: {
     width: "520px",
     height: "194px",
-    // display: "flex",
-    // alignItems: "center",
-    // justifyContent: "center",
+    opacity: "0.5",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      height: "50%",
+      marginTop: "-12rem",
+      display: "none"
+    },
   },
-  // flowerContainer: {
-  //   width: "520px",
-  //   height: "194px",
-  // },
   flowerImg: {
     width: "100%",
     height: "100%",
     objectFit: "fill",
+  },
+  gridContainer: {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gridGap: "2rem",
+    width: "80%",
+    marginLeft: "10%",
+    [theme.breakpoints.down("sm")]: {
+      gridTemplateColumns: "repeat(2, 1fr)",
+    },
+    [theme.breakpoints.down("xs")]: {
+      gridTemplateColumns: "1fr",
+    },
   }
 }));
-
 
 const List = () => {
   const classes = useStyles();
   const [openModal, setOpenModal] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [presentsList, setPresentsList] = useState([]);
-  const [selectedGift, setSelectedGift] = useState({id: '', name: '', link: ''});
+  const [selectedGift, setSelectedGift] = useState({ id: '', name: '', link: '', image: '' }); // Adicionado image ao estado
   const [openSuccessModal, setOpenSuccessModal] = useState(false);
   const [hasGiftsAvailable, setHasGiftsAvailable] = useState(true);
   const [ipInfo, setIpInfo] = useState({});
@@ -142,24 +183,24 @@ const List = () => {
 
   const listGiftsInitial = () => {
     setInputValue("");
-    setLoading(false)
+    setLoading(false);
     listGiftsByStatus('AVAILABLE')
       .then(response => {
         setLoadingMain(false);
         if (response?.length) {
           setPresentsList(response);
-          setHasGiftsAvailable(true)
+          setHasGiftsAvailable(true);
         } else {
           setPresentsList([]);
-          setHasGiftsAvailable(false)
+          setHasGiftsAvailable(false);
         }
       })
       .catch((err) => {
         setLoadingMain(false);
         setPresentsList([]);
-      })
-  }
-    
+      });
+  };
+
   const handleSelectPresent = () => {
     setLoading(true);
     selectGift(selectedGift.id, inputValue, byLink, ipInfo).then(response => {
@@ -171,11 +212,11 @@ const List = () => {
 
       window.open(selectedGift.link, '_blank');
       listGiftsInitial();
-    })
+    });
   };
-  
-  const handleOpenModal = (id, name, link, byLink = false) => {
-    setSelectedGift({id, name, link});
+
+  const handleOpenModal = (id, name, link, image, byLink = false) => { // Adicionado image como parâmetro
+    setSelectedGift({ id, name, link, image });
     setByLink(byLink);
     setOpenModal(true);
   };
@@ -187,63 +228,55 @@ const List = () => {
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
-  
+
   return (
     <div style={{ padding: "2rem 1rem" }}>
-      
-      <div style={{display: "grid", gridTemplateColumns: "repeat(2)", alignItems: "center", justifyContent: "center"}}>
-      <div className={classes.flowerDiv}>
+       <div className={classes.logoContainer}>
+        <div className={classes.flowerDiv}>
           <img style={{opacity: "0.5"}} className={classes.flowerImg} src='assets/flower.png' alt=""></img>
         </div>
-        <div className={classes.justifyCenter} style={{zIndex: "99", marginTop: "-10rem", marginLeft: "1rem"}}>
+        <div className={classes.justifyCenter} style={{zIndex: "99", marginTop: "-10rem",marginLeft: "1rem"}}>
           <p className={classes.title}>Presenteie Me </p>
           <p className={classes.subtitle}>Mateus e Patricia</p>
         </div>
-        
-      </div>
-      
-      
-      <div className={classes.descriptionDiv} >
-      <p style={{color: "#fff"}}>Querido convidado aqui consta nossa lista de presentes que só é possível de existir graças a sua presença, sinta-se a vontade de escolher algum desses itens abaixo e compra-lo pelo site ou pela loja de sua preferência. Em pouco tempo estaremos nos mudando para nossa casa nova e seu presente será de muita valia.</p>
-        
       </div>
 
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        {
-          loadingMain && <CircularProgress size={60} style={{ color: '#eda787' }} />
-          
-        }
+      <div className={classes.descriptionDiv}>
+        <p>Querido convidado, aqui consta nossa lista de presentes que só é possível existir graças à sua presença. Sinta-se à vontade para escolher algum desses itens abaixo e comprá-lo pelo site ou pela loja de sua preferência. Em pouco tempo estaremos nos mudando para nossa casa nova e seu presente será de muita valia.</p>
       </div>
-      {
-        !loadingMain && 
+
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        {loadingMain && <CircularProgress size={60} style={{ color: '#eda787' }} />}
+      </div>
+
+      {!loadingMain && (
         <div className={classes.gridContainer}>
-        {presentsList.map((present) => (
-          <div key={present.id} className={classes.root}>
-            <div className={classes.imgContainer}>
-              <img className={classes.img} src={present.image} alt="" />
-            </div>
-            <div>
-              <div className={classes.name}>{present.name}</div>
-              <div className={classes.price}>R$ {present.price}</div>
+          {presentsList.map((present) => (
+            <div key={present.id} className={classes.root}>
+              <div className={classes.imgContainer}>
+                <img className={classes.img} src={present.image} alt="" />
+              </div>
+              <div>
+                <div className={classes.name}>{present.name}</div>
+                <div className={classes.price}>R$ {present.price}</div>
+              </div>
               <div className={classes.buttonGroup}>
                 <Button
                   className={classes.linkBtn}
                   variant="contained"
-                  onClick={() => {handleOpenModal(present.id, present.name, present.link, true)}}
+                  onClick={() => { handleOpenModal(present.id, present.name, present.link, present.image, true) }}
                 >
                   Comprar no site
                 </Button>
-                <Button className={classes.boughtBtn} variant="contained" onClick={() => handleOpenModal(present.id, present.name, present.link)}>
+                <Button className={classes.boughtBtn} variant="contained" onClick={() => handleOpenModal(present.id, present.name, present.link, present.image)}>
                   Comprar presencialmente
                 </Button>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      }
+          ))}
+        </div>
+      )}
 
-      
       <Modal
         open={openModal}
         onClose={handleCloseModal}
@@ -282,12 +315,17 @@ const List = () => {
           >
             x
           </div>
+          {selectedGift.image && ( // Verifica se há uma imagem selecionada
+            <div className={classes.imgContainer} style={{marginTop: "1.5rem"}}>
+              <img className={classes.img} src={selectedGift.image} alt="" />
+            </div>
+          )}
           <div>
-            <h4 style={{color: "#fff"}}>Você escolheu comprar o presente <span style={{margin: "0.6rem 0 -0.5rem 0", color: "#eda787", fontStyle: "italic", fontWeight: "bold"}}>{selectedGift.name}</span>, se estiver correto: </h4>
+            <h4 style={{ color: "#fff" }}>Você escolheu comprar o presente <span style={{ margin: "0.6rem 0 -0.5rem 0", color: "#eda787", fontStyle: "italic", fontWeight: "bold" }}>{selectedGift.name}</span> {!byLink ? 'presencialemnte, ' : 'pelo site, '} se estiver correto: </h4>
           </div>
           <div
             style={{
-              width: "80%",
+              width: "90%",
               display: "flex",
               alignItems: "center",
               justifyContent: "flex-start"
@@ -298,7 +336,6 @@ const List = () => {
               style={{
                 fontSize: "1rem",
                 color: "#eda787",
-                marginBottom: "0.2rem",
                 display: "flex",
                 alignItems: "flex-start",
               }}
@@ -306,10 +343,10 @@ const List = () => {
               Digite seu nome:
             </label>
           </div>
-          
+
           <div
             style={{
-              width: "80%",
+              width: "90%",
               display: "flex",
               alignItems: "center",
               marginBottom: "1rem",
@@ -323,6 +360,7 @@ const List = () => {
               onChange={handleInputChange}
               style={{
                 width: "100%",
+                height: "15px",
                 padding: "0.5rem",
                 borderRadius: "5px",
                 border: "none",
@@ -336,12 +374,10 @@ const List = () => {
               onClick={() => handleSelectPresent()}
               style={{ marginLeft: "1rem", minWidth: "70px" }}
             >
-              {
-                loading 
-                  ? <CircularProgress size={24} style={{ color: '#FFFFFF' }} /> 
-                  : byLink ? "Ir para o site" : "Comprar presencialmente"
+              {loading
+                ? <CircularProgress size={24} style={{ color: '#FFFFFF' }} />
+                : byLink ? "Ir para o site" : "Comprar"
               }
-              
             </Button>
           </div>
           <div
@@ -358,10 +394,7 @@ const List = () => {
 
       <ModalSuccess selectedGift={selectedGift.name} open={openSuccessModal} onClose={() => setOpenSuccessModal(false)}></ModalSuccess>
     </div>
-
   );
-  
-
 };
 
 export default List;
